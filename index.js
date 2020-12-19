12f98ba14dc1ce9430054784d20164bb4bb2ce1e
ragapushadwa <ragapushadwa@gmail.com>
2020-12-19 14:11:20 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send({data:'success'})
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(3001)
