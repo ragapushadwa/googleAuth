03cf301a6e2bd4039d3aacac0ab472a007e7ef81
ragapushadwa <ragapushadwa@gmail.com>
2020-12-20 10:15:58 +0700

    push

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.export ={
+    googleClientID:'246150108979-9tr7meehpd94def9et6o1gfjdc54qest.apps.googleusercontent.com',
+    googleClientSecret:'XNBwBS9bSOB0oS5QQGS1sdMw'
+} 
--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express')
-
+const passport =require('passport')
+const GoogleStrategy = require ('passport-google-oauth20').Strategy
+const keys = require ('./config/keys')
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send({data:'success'})
-})
+passport.use(new GoogleStrategy({
+    clientID:keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+},
+accessToken=>{
+    console.log(accessToken)
+}
+)
+)
+
+//246150108979-9tr7meehpd94def9et6o1gfjdc54qest.apps.googleusercontent.com
+//XNBwBS9bSOB0oS5QQGS1sdMw
+
+app.get('/auth/google',passport.authenticate('google',{
+    scope:['profile','email']
+}))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
