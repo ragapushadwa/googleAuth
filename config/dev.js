6a1153b7c658cbd61b1fb01eee09416808efc414
ragapushadwa <ragapushadwa@gmail.com>
2020-12-29 13:26:24 +0700

    finish auth flow

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports ={
+    googleClientID:'246150108979-9tr7meehpd94def9et6o1gfjdc54qest.apps.googleusercontent.com',
+     googleClientSecret:'XNBwBS9bSOB0oS5QQGS1sdMw',
+     mongoURI:'mongodb+srv://raga:raga@cluster0.45j3w.mongodb.net/raga?retryWrites=true&w=majority',
+     cookieKey:'da49iruwfbdbh00h83renfimod'
+     } 
--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,5 @@
-module.exports ={
-   googleClientID:'246150108979-9tr7meehpd94def9et6o1gfjdc54qest.apps.googleusercontent.com',
-    googleClientSecret:'XNBwBS9bSOB0oS5QQGS1sdMw',
-    mongoURI:'mongodb+srv://raga:raga@cluster0.45j3w.mongodb.net/raga?retryWrites=true&w=majority',
-    cookieKey:'da49iruwfbdbh00h83renfimod'
-    
-} 
+if (process.env.NODE_ENV==='production'){
+    module.exports=require('./prod')
+}else{
+    module.exports=require('./dev')
+}
