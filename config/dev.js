c33c07500015b1e559d45cae81140bea20f1a722
ragapushadwa <ragapushadwa@gmail.com>
2020-12-29 13:45:56 +0700

    benerin git ignore

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports ={
-    googleClientID:'246150108979-9tr7meehpd94def9et6o1gfjdc54qest.apps.googleusercontent.com',
-     googleClientSecret:'XNBwBS9bSOB0oS5QQGS1sdMw',
-     mongoURI:'mongodb+srv://raga:raga@cluster0.45j3w.mongodb.net/raga?retryWrites=true&w=majority',
-     cookieKey:'da49iruwfbdbh00h83renfimod'
-     } 
